ff1bb704bd30b99e69a05ed7351f9b1673deb7cb
Dipanshu <dip100shaw@gmail.com>
2024-03-18 20:25:24 +0530

    done

--- clock.js
+++ clock.js
@@ -3,9 +3,18 @@ setInterval(()=>{
     // console.log(date);
     time = date.getSeconds();
     clockContainer = document.getElementById("clockContainer");
-    hour.innerHTML = date.getHours() + " :";
-    minute.innerHTML = date.getMinutes() + " :";
+    hour.innerHTML = date.getHours() + ":";
+    minute.innerHTML = date.getMinutes() + ":";
     second.innerHTML = date.getSeconds();
-
+    days = ['Sunday', 'Monday', 'tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    // console.log();
+    day.innerText = days[date.getDay()];
+    dates.innerText = date.getDate() + " " + months[date.getMonth()]+ ", ";
+    year.innerText = date.getFullYear();
 },1000)
+
+// date = new Date;
+// console.log(date);
+
 
